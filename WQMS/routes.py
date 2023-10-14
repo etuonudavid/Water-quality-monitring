5c7e291b84b506fc924e566ba094746401ba4d2a
davidetuonu <davidetuonu12@gmail.com>
2023-10-14 16:46:46 +0100

    finished the export to csv functionality by adding a tag and also adding csv.js file

--- WQMS/routes.py
+++ WQMS/routes.py
@@ -1,4 +1,5 @@
-from flask import request, jsonify, render_template, url_for
+import pandas as pd
+from flask import request, jsonify, render_template, url_for, Response
 from WQMS import app, db
 from WQMS.Model import SensorData
 
@@ -56,3 +57,31 @@ def get_data():
         return jsonify(data_list), 200
     except Exception as e:
         return str(e), 400
+
+
+@app.route('/export_csv', methods=['GET'])
+def export_csv():
+    # Query the database to fetch data from the SensorData table
+    data = SensorData.query.all()
+
+    if not data:
+        return 'No data to export', 404
+
+    # Create a DataFrame from the data
+    data_dict = [{'timestamp': entry.timestamp, 'temperature': entry.temperature, 'tds': entry.tds, 'turbidity': entry.turbidity, 'ph': entry.ph} for entry in data]
+    df = pd.DataFrame(data_dict)
+
+    # Convert the DataFrame to CSV
+    csv_data = df.to_csv(index=False)
+
+    # Create a Response object and set the content type to CSV
+    response = Response(csv_data, content_type='text/csv')
+    response.headers["Content-Disposition"] = "attachment; filename=data.csv"
+
+    return response
+
+
+
+
+
+
---
+++ WQMS/static/js/csv.js
@@ -0,0 +1,17 @@
+document.getElementById('export-csv').addEventListener('click', function () {
+    fetch('http://127.0.0.1:5000//export_csv', {
+        method: 'GET'
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        const url = window.URL.createObjectURL(new Blob([blob]);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'data.csv';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+        console.log("helloo")
+    });
+});
